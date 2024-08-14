7cdbf6b30c7af69db979d9a9416150206c372d42
rezmes <rezmes@gmail.com>
2024-08-14 17:21:05 +0330

    input and select edit

--- src/webparts/projCostEst/components/ProjCostTable/NewItemForm.tsx
+++ src/webparts/projCostEst/components/ProjCostTable/NewItemForm.tsx
@@ -1,25 +1,37 @@
-import * as React from 'react';
-import styles from './NewItemForm.module.scss';
-import ProjCostDropBox from './ProjCostDropBox';
+import * as React from "react";
+import styles from "./NewItemForm.module.scss";
+import ProjCostDropBox from "./ProjCostDropBox";
 import { sp } from "@pnp/sp/presets/all"; // Import PnP JS
 
-
 interface INewItemFormProps {
-  newItem: { ItemName: string, PricePerUnit: number, itemNumber: number, ItemType: string, Days: number, Description: string };
-  handleNewItemChange: (field: keyof INewItemFormProps['newItem'], value: string | number) => void;
+  newItem: {
+    ItemName: string;
+    PricePerUnit: number;
+    itemNumber: number;
+    ItemType: string;
+    Days: number;
+    Description: string;
+  };
+  handleNewItemChange: (
+    field: keyof INewItemFormProps["newItem"],
+    value: string | number
+  ) => void;
   addItem: () => void;
   projCostResources: { ItemName: string; PricePerUnit: number }[]; // Add this line
 }
 
 interface INewItemFormState {
-  projCostResources: { ItemName: string, PricePerUnit: number }[];
+  projCostResources: { ItemName: string; PricePerUnit: number }[];
 }
 
-class NewItemForm extends React.Component<INewItemFormProps, INewItemFormState> {
+class NewItemForm extends React.Component<
+  INewItemFormProps,
+  INewItemFormState
+> {
   constructor(props: INewItemFormProps) {
     super(props);
     this.state = {
-      projCostResources: []
+      projCostResources: [],
     };
   }
 
@@ -40,17 +52,20 @@ class NewItemForm extends React.Component<INewItemFormProps, INewItemFormState> 
     }
   }
 
-  handleSelect = (item: { label: string, value: { ItemName: string, PricePerUnit: number } }) => {
-    this.props.handleNewItemChange('ItemName', item.value.ItemName);
-    this.props.handleNewItemChange('PricePerUnit', item.value.PricePerUnit);
+  handleSelect = (item: {
+    label: string;
+    value: { ItemName: string; PricePerUnit: number };
+  }) => {
+    this.props.handleNewItemChange("ItemName", item.value.ItemName);
+    this.props.handleNewItemChange("PricePerUnit", item.value.PricePerUnit);
   };
 
   render() {
     const { newItem, handleNewItemChange, addItem } = this.props;
     const { projCostResources } = this.state;
-    const dropBoxOptions = projCostResources.map(resource => ({
+    const dropBoxOptions = projCostResources.map((resource) => ({
       label: resource.ItemName,
-      value: resource
+      value: resource,
     }));
     //log(dropBoxOptions);
     // console.log(dropBoxOptions);  // <-- Add this line
@@ -62,7 +77,7 @@ class NewItemForm extends React.Component<INewItemFormProps, INewItemFormState> 
           <ProjCostDropBox
             options={dropBoxOptions}
             value={newItem.ItemName}
-            onChange={(value) => handleNewItemChange('ItemName', value)}
+            onChange={(value) => handleNewItemChange("ItemName", value)}
             onSelect={this.handleSelect}
           />
         </td>
@@ -71,7 +86,9 @@ class NewItemForm extends React.Component<INewItemFormProps, INewItemFormState> 
             type="number"
             value={newItem.PricePerUnit}
             placeholder="New Price Per Unit"
-            onChange={(e) => handleNewItemChange('PricePerUnit', parseFloat(e.target.value))}
+            onChange={(e) =>
+              handleNewItemChange("PricePerUnit", parseFloat(e.target.value))
+            }
           />
         </td>
         <td>
@@ -79,11 +96,17 @@ class NewItemForm extends React.Component<INewItemFormProps, INewItemFormState> 
             type="number"
             value={newItem.itemNumber}
             placeholder="New Item Number"
-            onChange={(e) => handleNewItemChange('itemNumber', parseInt(e.target.value))}
+            onChange={(e) =>
+              handleNewItemChange("itemNumber", parseInt(e.target.value))
+            }
           />
         </td>
         <td>
-          <select value={newItem.ItemType} onChange={(e) => handleNewItemChange('ItemType', e.target.value)}>
+          <select
+            aria-label="Type of options"
+            value={newItem.ItemType}
+            onChange={(e) => handleNewItemChange("ItemType", e.target.value)}
+          >
             <option value="کالای مصرفی">کالای مصرفی</option>
             <option value="نیروی انسانی">نیروی انسانی</option>
             <option value="دستگاه">دستگاه</option>
@@ -94,7 +117,9 @@ class NewItemForm extends React.Component<INewItemFormProps, INewItemFormState> 
             type="number"
             value={newItem.Days}
             placeholder="Days"
-            onChange={(e) => handleNewItemChange('Days', parseInt(e.target.value))}
+            onChange={(e) =>
+              handleNewItemChange("Days", parseInt(e.target.value))
+            }
           />
         </td>
         <td>
@@ -102,11 +127,13 @@ class NewItemForm extends React.Component<INewItemFormProps, INewItemFormState> 
             type="text"
             value={newItem.Description}
             placeholder="Description"
-            onChange={(e) => handleNewItemChange('Description', e.target.value)}
+            onChange={(e) => handleNewItemChange("Description", e.target.value)}
           />
         </td>
         <td>
-          <button aria-label="Add" onClick={addItem}>Add</button>
+          <button aria-label="Add" onClick={addItem}>
+            Add
+          </button>
         </td>
       </tr>
     );
--- src/webparts/projCostEst/components/ProjCostTable/ProjCostTableRow.tsx
+++ src/webparts/projCostEst/components/ProjCostTable/ProjCostTableRow.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import styles from './ProjCostTableRow.module.scss';
-import ModifiedBy from './ModifiedBy';
-import ItemType from './ItemType';
-import Days from './Days';
-import Description from './Description';
+import * as React from "react";
+import styles from "./ProjCostTableRow.module.scss";
+import ModifiedBy from "./ModifiedBy";
+import ItemType from "./ItemType";
+import Days from "./Days";
+import Description from "./Description";
 
 interface IProjCostTableRowProps {
   index: number;
@@ -30,7 +30,10 @@ interface IProjCostTableRowProps {
   };
   isSelected: boolean;
   toggleSelectItem: (index: number) => void;
-  handleChange: (field: keyof IProjCostTableRowProps['editedValues'], value: string | number) => void;
+  handleChange: (
+    field: keyof IProjCostTableRowProps["editedValues"],
+    value: string | number
+  ) => void;
   projCostResources: { ItemName: string; PricePerUnit: number }[]; // Add this
 }
 
@@ -42,57 +45,90 @@ const ProjCostTableRow: React.StatelessComponent<IProjCostTableRowProps> = ({
   isSelected,
   toggleSelectItem,
   handleChange,
-  projCostResources // Add this line
+  projCostResources, // Add this line
 }) => {
   return (
     <tr className={styles.projCostTableRow}>
       <td>
-        <input
-          type="checkbox"
-          checked={isSelected}
-          onChange={() => toggleSelectItem(index)}
-        />
+        <label>
+          تیک:
+          <input
+            type="checkbox"
+            checked={isSelected}
+            onChange={() => toggleSelectItem(index)}
+          />
+        </label>
       </td>
       <td>
         {isEditing ? (
-          <input
-            type="text"
-            value={editedValues.ItemName}
-            onChange={(e) => handleChange('ItemName', e.target.value)}
-          />
+          <label htmlFor="">
+            متن{" "}
+            <input
+              type="text"
+              value={editedValues.ItemName}
+              onChange={(e) => handleChange("ItemName", e.target.value)}
+            />
+          </label>
         ) : (
           item.ItemName
         )}
       </td>
       <td>
         {isEditing ? (
-          <input
-            type="number"
-            value={editedValues.PricePerUnit}
-            onChange={(e) => handleChange('PricePerUnit', Number(e.target.value))}
-          />
+          <label htmlFor="">
+            عدد
+            <input
+              type="number"
+              value={editedValues.PricePerUnit}
+              onChange={(e) =>
+                handleChange("PricePerUnit", Number(e.target.value))
+              }
+            />
+          </label>
         ) : (
-          item.PricePerUnit.toLocaleString('fa-IR', { style: 'currency', currency: 'IRR', minimumFractionDigits: 0 })
+          item.PricePerUnit.toLocaleString("fa-IR", {
+            style: "currency",
+            currency: "IRR",
+            minimumFractionDigits: 0,
+          })
         )}
       </td>
       <td>
         {isEditing ? (
-          <input
-            type="number"
-            value={editedValues.itemNumber}
-            onChange={(e) => handleChange('itemNumber', Number(e.target.value))}
-          />
+          <label htmlFor="">
+            عدد
+            <input
+              type="number"
+              value={editedValues.itemNumber}
+              onChange={(e) =>
+                handleChange("itemNumber", Number(e.target.value))
+              }
+            />
+          </label>
         ) : (
           item.itemNumber
         )}
       </td>
       <td>
-        {item.TotalPrice.toLocaleString('fa-IR', { style: 'currency', currency: 'IRR', minimumFractionDigits: 0 })}
+        {item.TotalPrice.toLocaleString("fa-IR", {
+          style: "currency",
+          currency: "IRR",
+          minimumFractionDigits: 0,
+        })}
       </td>
       <ModifiedBy modifiedBy={item.Editor} />
-      <ItemType ItemType={item.ItemType} handleChange={(e) => handleChange('ItemType', e.target.value)} />
-      <Days days={item.Days} handleChange={(e) => handleChange('Days', Number(e.target.value))} />
-      <Description description={item.Description} handleChange={(e) => handleChange('Description', e.target.value)} />
+      <ItemType
+        ItemType={item.ItemType}
+        handleChange={(e) => handleChange("ItemType", e.target.value)}
+      />
+      <Days
+        days={item.Days}
+        handleChange={(e) => handleChange("Days", Number(e.target.value))}
+      />
+      <Description
+        description={item.Description}
+        handleChange={(e) => handleChange("Description", e.target.value)}
+      />
     </tr>
   );
 };
